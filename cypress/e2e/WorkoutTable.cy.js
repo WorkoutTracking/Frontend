5a9f88603dd45cc9acab85f6a58b2de9d252c923
Carlo van Kessel <carlovankessel@yahoo.nl>
2022-12-12 15:18:07 +0100

    Add cypress env and add cypress to tests with docker image

--- cypress/e2e/WorkoutTable.cy.js
+++ cypress/e2e/WorkoutTable.cy.js
@@ -3,10 +3,10 @@ describe('WorkoutTable', () => {
     cy.visit('http://localhost:8081/')
 
     cy.get('#username')
-        .type('carlovankessel@yahoo.nl').should('have.value', 'carlovankessel@yahoo.nl')
+        .type(Cypress.env('CYPRESS_USERNAME'))
 
     cy.get('#password')
-        .type('admin').should('have.value', 'admin')
+        .type(Cypress.env('CYPRESS_PASSWORD'))
 
     cy.get('#kc-form-login').submit()
   })
@@ -23,45 +23,4 @@ describe('WorkoutTable', () => {
 
     cy.get('h1').should('have.text', 'Workout: Legs')
   });
-
-  it('should add exercise to Legs workout', function () {
-    cy.get('.editWorkoutButton').first().click()
-    cy.get('h1').should('have.text', 'Workout: Legs')
-
-    cy.get('#name')
-        .type('Squat')
-
-    cy.get('form').submit()
-  });
-
-  it('should add set to workout', function () {
-    cy.get('.editWorkoutButton').first().click()
-    cy.get('.addSet').click()
-  });
-
-  it('should update set', function () {
-    cy.get('.editWorkoutButton').first().click()
-    cy.wait(1000)
-
-    cy.get('.setsInput')
-        .type('2')
-
-    cy.get('.repsInput')
-        .type('8')
-
-    cy.get('.weightInput')
-        .type('120')
-
-    cy.get('.restInput')
-        .type('5')
-
-    cy.get('.updateSet').submit()
-
-    cy.get('.setsInput').should('have.value', '2')
-    cy.get('.repsInput').should('have.value', '8')
-    cy.get('.weightInput').should('have.value', '120')
-    cy.get('.restInput').should('have.value', '5')
-
-  });
-
 })
